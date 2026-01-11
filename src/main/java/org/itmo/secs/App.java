bb2ee5defe5dfd2ffc5cb84eaaca7b56fe5e1bfc
Ivan Marukhlenko <ivan.marukh@gmail.com>
2026-01-11 16:55:32 +0300

    Добавлена ошибка для проверки SAST

--- src/main/java/org/itmo/secs/App.java
+++ src/main/java/org/itmo/secs/App.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class App {
+    public static int f(Object obj) {
+        return obj.hashCode();
+    }
     public static void main(String[] args) {
+        f(null);
         SpringApplication.run(App.class, args);
     }
 }
